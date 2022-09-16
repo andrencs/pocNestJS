57a835a0467c7a9bac10c5004c3213971d26ec7c
André Nunes Corrêa da Silva <andre-ncs@hotmail.com>
2022-09-16 13:48:35 -0300

    User CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/test`),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
